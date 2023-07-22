c91f8d1061632adccc96decdf3d85a525e5a430d
nnicholson95 <natenicholson95@gmail.com>
2023-07-22 17:17:26 -0500

    Added a Hitscan weapon class and pistol that uses it

--- Source/Blaster/Weapon/WeaponTypes.h
+++ Source/Blaster/Weapon/WeaponTypes.h
@@ -10,6 +10,7 @@ enum class EWeaponType : uint8
 {
 	EWT_AssaultRifle UMETA(DisplayName = "AssaultRifle"),
 	EWT_RocketLauncher UMETA(DisplayName = "RocketLauncher"),
+	EWT_Pistol UMETA(DisplayName = "Pistol"),
 
 	EWT_MAX UMETA(DisplayName = "DefaultMax")
 };
