ab029a9ce660406ce3a920f87830a2c4b35b2af2
nnicholson95 <natenicholson95@gmail.com>
2023-07-29 16:49:28 -0500

    Added Health Pickup Class
    TODO make the health pickup heal the character

---
+++ Source/Blaster/Pickups/HealthPickup.cpp
@@ -0,0 +1,40 @@
+#include "HealthPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponents/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponenet"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool BFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, BFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+
+	}
+
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+
+	Super::Destroyed();
+}
