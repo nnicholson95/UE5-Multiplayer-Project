97d0d539b264309e99de136d5af997185b099b0d
nnicholson95 <natenicholson95@gmail.com>
2023-08-04 12:47:37 -0500

    Server Side Rewind Started

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -50,6 +50,75 @@ void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, c
 	}
 }
 
+/*
+* Rewinds the server side version of a shot character to ensure that hits are accurate in real world latency situations
+* 
+* Called by the character that did the shooting -- HitCharacter is the character that did the shooting
+*/
+void ULagCompensationComponent::ServerSideRewind(ABlasterCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime)
+{
+	bool bReturn = 
+		HitCharacter == nullptr ||
+		HitCharacter->GetLagCompensation() == nullptr ||
+		HitCharacter->GetLagCompensation()->FrameHistory.GetHead() == nullptr ||
+		HitCharacter->GetLagCompensation()->FrameHistory.GetTail() == nullptr;
+
+	//Frame package we check to verify a hit
+	FFramePackage FrameToCheck;
+
+	//Most of the time we will need to interpolate
+	bool bShouldInterpolate = true;
+
+	// Frame history of the HitCharacter
+	const TDoubleLinkedList<FFramePackage>& History = HitCharacter->GetLagCompensation()->FrameHistory;
+	const float OldestHistoryTime = History.GetTail()->GetValue().Time;
+	const float NewestHistoryTime = History.GetHead()->GetValue().Time;
+	if (OldestHistoryTime > HitTime)
+	{
+		// too far back - too laggy to do server side rewind
+		return;
+	}
+	if (OldestHistoryTime == HitTime)
+	{
+		//We found the frame as well but it will be the tail
+		FrameToCheck = History.GetTail()->GetValue();
+		bShouldInterpolate = false;
+	}
+	if (NewestHistoryTime <= HitTime)
+	{
+		//This means we found the frame to check 
+		FrameToCheck = History.GetHead()->GetValue();
+		bShouldInterpolate = false;
+	}
+
+	TDoubleLinkedList<FFramePackage>::TDoubleLinkedListNode* Younger = History.GetHead();
+	TDoubleLinkedList<FFramePackage>::TDoubleLinkedListNode* Older = Younger;
+	while (Older->GetValue().Time > HitTime) //Is Older still younger than HItTime?
+	{
+		//March back until OlderTime < HitTime < YoungerTime
+		if (Older->GetNextNode() == nullptr) break;
+		Older = Older->GetNextNode();
+		//We want to move younger to the same node as older but only if it is before hit time
+		if (Older->GetValue().Time > HitTime)
+		{
+			Younger = Older;
+		}
+	}
+	//Highly unlikely, but after me moved our pointer we found out frame to check 
+	if (Older->GetValue().Time == HitTime)
+	{
+		FrameToCheck = Older->GetValue();
+		bShouldInterpolate = false;
+	}
+
+	if (bShouldInterpolate)
+	{
+		//Interpolate between younger and older
+	}
+
+	if (bReturn) return;
+}
+
 void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
