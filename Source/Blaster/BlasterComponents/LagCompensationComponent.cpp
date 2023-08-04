0ec4e758dced0546fd591f3e7e9f24eb9de8ec19
nnicholson95 <natenicholson95@gmail.com>
2023-08-04 11:56:12 -0500

    Storing Frame History
    Using a double Linked list we can easily manage a frame history by shaving off tail nodes as we add new ones.

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -12,10 +12,6 @@ ULagCompensationComponent::ULagCompensationComponent()
 void ULagCompensationComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	FFramePackage Package;
-	SaveFramePackage(Package);
-	ShowFramePackage(Package, FColor::Orange);
 }
 
 void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
@@ -48,7 +44,8 @@ void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, c
 			BoxInfo.Value.BoxExtent,
 			FQuat(BoxInfo.Value.Rotation),
 			Color,
-			true
+			false,
+			4.f
 		);
 	}
 }
@@ -57,4 +54,27 @@ void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	if (FrameHistory.Num() <= 1)
+	{
+		FFramePackage ThisFrame;
+		SaveFramePackage(ThisFrame);
+		FrameHistory.AddHead(ThisFrame);
+	}
+	else
+	{
+		//Time of the newest frame - time of the oldest frame
+		float HistoryLength = FrameHistory.GetHead()->GetValue().Time - FrameHistory.GetTail()->GetValue().Time;
+		//Remove tail until frame history is desired length
+		while (HistoryLength > MaxRecordTime)
+		{
+			//Remove the oldest frame -- but doesn't necessarily mean that our history length < MaxRecordTime
+			FrameHistory.RemoveNode(FrameHistory.GetTail());
+			HistoryLength = FrameHistory.GetHead()->GetValue().Time - FrameHistory.GetTail()->GetValue().Time;
+		}
+		FFramePackage ThisFrame;
+		SaveFramePackage(ThisFrame);
+		FrameHistory.AddHead(ThisFrame);
+
+		ShowFramePackage(ThisFrame, FColor::Red);
+	}
 }
